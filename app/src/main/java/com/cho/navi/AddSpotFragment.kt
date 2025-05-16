4e5e1a302c11511d538cdd5a86fec3522e411ba6
mandol96 <chocan0202@gmail.com>
2025-05-16 09:25:28 +0900

    입력 값 유무에 따른 버튼 활성화

--- app/src/main/java/com/cho/navi/AddSpotFragment.kt
+++ app/src/main/java/com/cho/navi/AddSpotFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import com.cho.navi.databinding.FragmentAddSpotBinding
 
@@ -12,6 +13,10 @@ class AddSpotFragment : Fragment() {
 	private var _binding: FragmentAddSpotBinding? = null
 	private val binding get() = _binding!!
 
+	private var isValidSpotCategory = false
+	private var isValidSpotName = false
+	private var isValidSpotDescription = false
+
 	override fun onCreateView(
 		inflater: LayoutInflater,
 		container: ViewGroup?,
@@ -19,6 +24,30 @@ class AddSpotFragment : Fragment() {
 	): View {
 		_binding = FragmentAddSpotBinding.inflate(inflater, container, false)
 		return binding.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		binding.etSpotName.doAfterTextChanged {
+			val spotName = it?.toString() ?: ""
+			isValidSpotName = spotName.isNotBlank()
+			updateButtonEnabledState()
+		}
+		binding.etSpotDescription.doAfterTextChanged {
+			val spotDescription = it?.toString() ?: ""
+			isValidSpotDescription = spotDescription.isNotBlank()
+			updateButtonEnabledState()
+		}
+		binding.autoCompleteTvSpotCategory.doAfterTextChanged {
+			val spotCategory = it.toString()
+			isValidSpotCategory = spotCategory.isNotBlank()
+			updateButtonEnabledState()
+		}
+	}
+
+	private fun updateButtonEnabledState() {
+		binding.btnConfirm.isEnabled =
+			isValidSpotName && isValidSpotDescription && isValidSpotCategory
 	}
 
 	override fun onDestroyView() {
