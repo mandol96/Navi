25bf356750d574b479da18242abc7192e7238983
mandol96 <chocan0202@gmail.com>
2025-05-16 14:37:53 +0900

    코드 정렬

--- app/src/main/java/com/cho/navi/MapFragment.kt
+++ app/src/main/java/com/cho/navi/MapFragment.kt
@@ -16,56 +16,56 @@ import java.lang.Exception
 
 class MapFragment : Fragment() {
 
-	private var _binding: FragmentMapBinding? = null
-	private val binding get() = _binding!!
+    private var _binding: FragmentMapBinding? = null
+    private val binding get() = _binding!!
 
-	private var map: KakaoMap? = null
+    private var map: KakaoMap? = null
 
-	override fun onCreateView(
-		inflater: LayoutInflater,
-		container: ViewGroup?,
-		savedInstanceState: Bundle?
-	): View {
-		_binding = FragmentMapBinding.inflate(inflater, container, false)
-		return binding.root
-	}
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-		super.onViewCreated(view, savedInstanceState)
-		val mapView = binding.mapView
-		mapView.start(
-			object : MapLifeCycleCallback() {
-				override fun onMapDestroy() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapView = binding.mapView
+        mapView.start(
+            object : MapLifeCycleCallback() {
+                override fun onMapDestroy() {
 
-				}
+                }
 
-				override fun onMapError(error: Exception?) {
-					error?.printStackTrace()
-				}
-			}, object : KakaoMapReadyCallback() {
-				override fun onMapReady(kakaoMap: KakaoMap) {
-					map = kakaoMap
-				}
-			}
-		)
+                override fun onMapError(error: Exception?) {
+                    error?.printStackTrace()
+                }
+            }, object : KakaoMapReadyCallback() {
+                override fun onMapReady(kakaoMap: KakaoMap) {
+                    map = kakaoMap
+                }
+            }
+        )
 
-		binding.fabAddSpot.setOnClickListener {
-			findNavController().navigate(R.id.action_map_to_addSpot)
-		}
-	}
+        binding.fabAddSpot.setOnClickListener {
+            findNavController().navigate(R.id.action_map_to_addSpot)
+        }
+    }
 
-	override fun onResume() {
-		super.onResume()
-		binding.mapView.resume()
-	}
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.resume()
+    }
 
-	override fun onPause() {
-		super.onPause()
-		binding.mapView.pause()
-	}
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.pause()
+    }
 
-	override fun onDestroyView() {
-		super.onDestroyView()
-		_binding = null
-	}
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
