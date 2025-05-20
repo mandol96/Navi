4f2f040b76397a59fdd702bd93dc71df80fc739a
mandol96 <chocan0202@gmail.com>
2025-05-20 16:58:08 +0900

    기능 : 입력란을 모두 기입해야 버튼 활성화

--- app/src/main/java/com/cho/navi/ui/addpost/AddPostFragment.kt
+++ app/src/main/java/com/cho/navi/ui/addpost/AddPostFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import com.cho.navi.databinding.FragmentAddPostBinding
 
@@ -12,6 +13,10 @@ class AddPostFragment : Fragment() {
     private var _binding: FragmentAddPostBinding? = null
     private val binding get() = _binding!!
 
+    private var isValidPostCategory = false
+    private var isValidPostTitle = false
+    private var isValidPostDescription = false
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -19,6 +24,44 @@ class AddPostFragment : Fragment() {
     ): View {
         _binding = FragmentAddPostBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setLayout()
+    }
+
+    private fun setLayout() {
+        setDropDownMenu()
+        setTextField()
+    }
+
+    private fun setDropDownMenu() {
+        binding.autoCompleteTvAddPostCategory.doAfterTextChanged {
+            val postCategory = it.toString()
+            isValidPostCategory = postCategory.isNotBlank()
+            updateButtonEnabledState()
+        }
+    }
+
+    private fun setTextField() {
+        with(binding) {
+            etPostTitle.doAfterTextChanged {
+                val postTitle = it?.toString() ?: ""
+                isValidPostTitle = postTitle.isNotBlank()
+                updateButtonEnabledState()
+            }
+            etPostDescription.doAfterTextChanged {
+                val postDescription = it?.toString() ?: ""
+                isValidPostDescription = postDescription.isNotBlank()
+                updateButtonEnabledState()
+            }
+        }
+    }
+
+    private fun updateButtonEnabledState() {
+        binding.btnConfirm.isEnabled =
+            isValidPostCategory && isValidPostTitle && isValidPostDescription
     }
 
     override fun onDestroyView() {
