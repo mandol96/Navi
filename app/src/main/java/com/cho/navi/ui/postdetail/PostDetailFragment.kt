baa3b4c121f94c5c2dd3775ef2e03e3385a453ed
mandol96 <chocan0202@gmail.com>
2025-05-21 12:52:09 +0900

    기능: 뒤로가기 구현

--- app/src/main/java/com/cho/navi/ui/postdetail/PostDetailFragment.kt
+++ app/src/main/java/com/cho/navi/ui/postdetail/PostDetailFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.cho.navi.databinding.FragmentPostDetailBinding
 
@@ -33,6 +34,9 @@ class PostDetailFragment : Fragment() {
             tvPostDetailTitle.text = post.title
             tvPostDetailDescription.text = post.description
             rvPostDetailImage.adapter = adapter
+            toolbarPostDetail.setNavigationOnClickListener {
+                findNavController().navigateUp()
+            }
         }
 
         adapter.addImages(post.imageUrls)
