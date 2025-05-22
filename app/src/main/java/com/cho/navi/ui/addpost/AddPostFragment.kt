7007f666f5f7fecb2fad171cbbce47f30b269ea6
mandol96 <chocan0202@gmail.com>
2025-05-22 17:54:56 +0900

    기능: 버튼 클릭시 firestore에 저장

--- app/src/main/java/com/cho/navi/ui/addpost/AddPostFragment.kt
+++ app/src/main/java/com/cho/navi/ui/addpost/AddPostFragment.kt
@@ -1,13 +1,18 @@
 package com.cho.navi.ui.addpost
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.cho.navi.databinding.FragmentAddPostBinding
+import com.google.firebase.Firebase
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.firestore
 
 class AddPostFragment : Fragment() {
 
@@ -17,6 +22,8 @@ class AddPostFragment : Fragment() {
     private var isValidPostCategory = false
     private var isValidPostTitle = false
     private var isValidPostDescription = false
+
+    val db = Firebase.firestore
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,6 +44,28 @@ class AddPostFragment : Fragment() {
         setTextField()
         binding.toolbarAddPost.setNavigationOnClickListener {
             findNavController().navigateUp()
+        }
+        binding.btnConfirm.setOnClickListener {
+            val postCategory = binding.autoCompleteTvAddPostCategory.text.toString()
+            val postTitle = binding.etPostTitle.text.toString()
+            val postDescription = binding.etPostDescription.text.toString()
+
+            val postData = hashMapOf(
+                "category" to postCategory,
+                "title" to postTitle,
+                "description" to postDescription,
+                "createdAt" to Timestamp.now()
+            )
+
+            db.collection("posts")
+                .add(postData)
+                .addOnSuccessListener {
+                    Toast.makeText(requireContext(), "게시글이 저장되었습니다.", Toast.LENGTH_SHORT).show()
+                    findNavController().navigateUp()
+                }
+                .addOnFailureListener {e ->
+                    Toast.makeText(requireContext(), "저장 실패: ${e.message}", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 
