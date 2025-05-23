35acb4e67b10b2688bb911d1446e526116e1258c
mandol96 <chocan0202@gmail.com>
2025-05-23 11:50:36 +0900

    수정: Post data class 파라미터 추가

--- app/src/main/java/com/cho/navi/data/Post.kt
+++ app/src/main/java/com/cho/navi/data/Post.kt
@@ -6,8 +6,9 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Post(
     val id: String,
+    val category: Category,
     val imageUrls: List<String>,
     val title: String,
     val description: String,
-    val location: String,
+    val location: String?,
 ) : Parcelable
