ba07e927503170f5885954d339c5de0c4d5bf8a6
mandol96 <chocan0202@gmail.com>
2025-05-23 14:48:53 +0900

    수정: Post 클래스 수정에 따른 코드 수정

--- app/src/main/java/com/cho/navi/data/Post.kt
+++ app/src/main/java/com/cho/navi/data/Post.kt
@@ -5,9 +5,9 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Post(
-    val id: String,
-    val category: Category,
-    val imageUrls: List<String>,
+    val id: String?,
+    val category: String,
+    val imageUrls: List<String>?,
     val title: String,
     val description: String,
     val location: String?,
--- app/src/main/java/com/cho/navi/data/Storage.kt
+++ app/src/main/java/com/cho/navi/data/Storage.kt
@@ -6,9 +6,9 @@ object Storage {
 
     private fun getDummyData(): List<Post> {
         return listOf(
-            Post("1", Category.PLAY, listOf("#000000","#facfac"), "게시글1", "내용1", "ㅇㅇ시ㅇㅇ구ㅇㅇ동"),
-            Post("2", Category.RESTAURANT, listOf("#a00faa","#EEC08A"), "게시글2", "내용2", "ㅇㅇ시ㅇㅇ구ㅇㅇ동"),
-            Post("3", Category.RESTAURANT, listOf("#966363","#2395C5"), "게시글3", "내용3", "ㅇㅇ시ㅇㅇ구ㅇㅇ동")
+            Post("1", "풍경", listOf("#000000", "#facfac"), "게시글1", "내용1", "ㅇㅇ시ㅇㅇ구ㅇㅇ동"),
+            Post("2", "놀이", listOf("#a00faa", "#EEC08A"), "게시글2", "내용2", "ㅇㅇ시ㅇㅇ구ㅇㅇ동"),
+            Post("3", "맛집", listOf("#966363", "#2395C5"), "게시글3", "내용3", "ㅇㅇ시ㅇㅇ구ㅇㅇ동")
         )
     }
 }
