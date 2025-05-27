562fbed884f56174bb2473f8eed517c4d6442733
mandol96 <chocan0202@gmail.com>
2025-05-27 23:01:37 +0900

    기능: 지도 열기 클릭 시 현재 위치 띄우기

--- app/src/main/java/com/cho/navi/ui/addspot/SelectSpotFragment.kt
+++ app/src/main/java/com/cho/navi/ui/addspot/SelectSpotFragment.kt
@@ -1,16 +1,22 @@
 package com.cho.navi.ui.addspot
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.cho.navi.databinding.FragmentSelectSpotBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.LatLng
 import com.kakao.vectormap.MapLifeCycleCallback
-import java.lang.Exception
+import com.kakao.vectormap.camera.CameraUpdateFactory
 
 class SelectSpotFragment : Fragment() {
 
@@ -18,13 +24,14 @@ class SelectSpotFragment : Fragment() {
     private val binding get() = _binding!!
 
     private var map: KakaoMap? = null
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentSelectSpotBinding.inflate(inflater,container,false)
+        _binding = FragmentSelectSpotBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -41,6 +48,7 @@ class SelectSpotFragment : Fragment() {
     }
 
     private fun setMap() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
         val mapView = binding.mapSelectSpot
         mapView.start(
             object : MapLifeCycleCallback() {
@@ -54,9 +62,36 @@ class SelectSpotFragment : Fragment() {
             }, object : KakaoMapReadyCallback() {
                 override fun onMapReady(kakaoMap: KakaoMap) {
                     map = kakaoMap
+                    currentLocation()
                 }
             }
         )
+    }
+
+    private fun currentLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                100
+            )
+            return
+        }
+
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            location?.let {
+                val lat = it.latitude
+                val lng = it.longitude
+
+                val latLng = LatLng.from(lat, lng)
+                val cameraUpdate = CameraUpdateFactory.newCenterPosition(latLng)
+                map?.moveCamera(cameraUpdate)
+            }
+        }
     }
 
     override fun onDestroyView() {
