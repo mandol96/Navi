c4bfc17b4978253ddbef1d7a412f169f225cd6e6
mandol96 <chocan0202@gmail.com>
2025-05-30 11:40:37 +0900

    수정: 코드 정렬

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,13 +2,13 @@ import java.io.FileInputStream
 import java.util.Properties
 
 plugins {
-	alias(libs.plugins.android.application)
-	alias(libs.plugins.kotlin.android)
-	alias(libs.plugins.android.navigation.safe.args.kotlin)
-	alias(libs.plugins.kotlin.parcelize)
-	alias(libs.plugins.google.services)
-	alias(libs.plugins.ksp)
-	alias(libs.plugins.hilt)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.android.navigation.safe.args.kotlin)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.google.services)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 val localPropertiesFile = rootProject.file("local.properties")
@@ -16,87 +16,87 @@ val properties = Properties()
 properties.load(FileInputStream(localPropertiesFile))
 
 android {
-	namespace = "com.cho.navi"
-	compileSdk = 35
+    namespace = "com.cho.navi"
+    compileSdk = 35
 
-	defaultConfig {
-		applicationId = "com.cho.navi"
-		minSdk = 24
-		targetSdk = 35
-		versionCode = 1
-		versionName = "1.0"
+    defaultConfig {
+        applicationId = "com.cho.navi"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
 
-		buildConfigField("String","KAKAO_API_KEY","\"${properties["kakaoapi.key"]}\"")
-		buildConfigField("String","KAKAO_REST_API_KEY","\"${properties["kakaorestapi.key"]}\"")
+        buildConfigField("String", "KAKAO_API_KEY", "\"${properties["kakaoapi.key"]}\"")
+        buildConfigField("String", "KAKAO_REST_API_KEY", "\"${properties["kakaorestapi.key"]}\"")
 
-		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-		ndk {
-			abiFilters.add("arm64-v8a")
-			abiFilters.add("armeabi-v7a")
-			abiFilters.add("x86")
-			abiFilters.add("x86_64")
-		}
-	}
+        ndk {
+            abiFilters.add("arm64-v8a")
+            abiFilters.add("armeabi-v7a")
+            abiFilters.add("x86")
+            abiFilters.add("x86_64")
+        }
+    }
 
-	buildTypes {
-		release {
-			isMinifyEnabled = false
-			proguardFiles(
-				getDefaultProguardFile("proguard-android-optimize.txt"),
-				"proguard-rules.pro"
-			)
-		}
-	}
-	compileOptions {
-		sourceCompatibility = JavaVersion.VERSION_11
-		targetCompatibility = JavaVersion.VERSION_11
-	}
-	kotlinOptions {
-		jvmTarget = "11"
-	}
-	buildFeatures {
-		viewBinding = true
-		buildConfig = true
-	}
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
 }
 
 dependencies {
 
-	implementation(libs.androidx.core.ktx)
-	implementation(libs.androidx.appcompat)
-	implementation(libs.material)
-	implementation(libs.androidx.activity)
-	implementation(libs.androidx.constraintlayout)
-	testImplementation(libs.junit)
-	androidTestImplementation(libs.androidx.junit)
-	androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 
-	//Jetpack
-	implementation(libs.androidx.fragment.ktx)
-	implementation(libs.androidx.navigation.fragment)
-	implementation(libs.androidx.navigation.ui)
+    //Jetpack
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
 
-	implementation(libs.kakao.maps)
-	implementation(libs.play.services.location)
+    implementation(libs.kakao.maps)
+    implementation(libs.play.services.location)
 
-	//Firebase
-	implementation(platform(libs.firebase.bom))
-	implementation(libs.firebase.analytics)
-	implementation(libs.firebase.firestore)
+    //Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.firestore)
 
-	//Hilt
-	implementation(libs.hilt.android)
-	ksp(libs.hilt.android.compiler)
+    //Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
 
-	//Network
-	implementation(platform(libs.okhttp.bom))
-	implementation(libs.okhttp)
-	implementation(libs.logging.interceptor)
-	implementation (libs.retrofit)
-	implementation (libs.converter.moshi)
+    //Network
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+    implementation(libs.retrofit)
+    implementation(libs.converter.moshi)
 
-	//Json
-	implementation(libs.moshi.kotlin)
-	ksp(libs.moshi.kotlin.codegen)
+    //Json
+    implementation(libs.moshi.kotlin)
+    ksp(libs.moshi.kotlin.codegen)
 }
