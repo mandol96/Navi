d3561e701e2cd8d57e71ecd726191d00e947fd9e
mandol96 <chocan0202@gmail.com>
2025-06-17 14:43:54 +0900

    수정: Action 변경 ResultAPI 활용

--- app/src/main/java/com/cho/navi/ui/addspot/SelectSpotFragment.kt
+++ app/src/main/java/com/cho/navi/ui/addspot/SelectSpotFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -64,8 +65,10 @@ class SelectSpotFragment : Fragment() {
         }
         binding.btnSelected.setOnClickListener {
             val address = binding.tvCurrentAddress.text.toString()
-            val action = SelectSpotFragmentDirections.actionSelectSpotToAddSpot(address)
-            findNavController().navigate(action)
+            parentFragmentManager.setFragmentResult("select_spot_result", bundleOf(
+                "selected_address" to address
+            ))
+            findNavController().navigateUp()
         }
     }
 
