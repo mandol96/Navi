62a4d80543301c563d2f39300091c6663d9e4422
mandol96 <chocan0202@gmail.com>
2025-06-17 22:04:23 +0900

    수정: Firestore 값 보여주기

--- app/src/main/java/com/cho/navi/ui/post/PostCategoryFragment.kt
+++ app/src/main/java/com/cho/navi/ui/post/PostCategoryFragment.kt
@@ -5,21 +5,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.cho.navi.PostClickListener
-import com.cho.navi.R
 import com.cho.navi.data.Category
 import com.cho.navi.data.Post
-import com.cho.navi.data.Storage
 import com.cho.navi.databinding.FragmentPostCategoryBinding
 import com.cho.navi.util.Constants
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class PostCategoryFragment : Fragment(), PostClickListener {
 
     private var _binding: FragmentPostCategoryBinding? = null
     private val binding get() = _binding!!
     private lateinit var category: Category
+    private val adapter = PostCategoryAdapter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +40,7 @@ class PostCategoryFragment : Fragment(), PostClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setLayout()
+        loadCategoryPosts()
     }
 
     private fun setCategory() {
@@ -49,8 +52,20 @@ class PostCategoryFragment : Fragment(), PostClickListener {
     }
 
     private fun setLayout() {
-        val adapter = PostCategoryAdapter(Storage.postList, this)
         binding.rvPost.adapter = adapter
+    }
+
+    private fun loadCategoryPosts() {
+        Firebase.firestore.collection("posts")
+            .whereEqualTo("category", category.displayName) // 예: "맛집"
+            .get()
+            .addOnSuccessListener { result ->
+                val posts = result.documents.mapNotNull { it.toObject(Post::class.java) }
+                adapter.submitList(posts) // 어댑터에 게시글 갱신
+            }
+            .addOnFailureListener {
+                Toast.makeText(requireContext(), "불러오기 실패", Toast.LENGTH_SHORT).show()
+            }
     }
 
     companion object {
