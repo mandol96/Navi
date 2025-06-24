db03d723ac9a18cb6b570cfb9b8f7d71898ee00c
mandol96 <chocan0202@gmail.com>
2025-06-24 21:26:34 +0900

    기능: 추천UI RecyclerView 구현

---
+++ app/src/main/java/com/cho/navi/ui/recommendation/RecommendationPostAdapter.kt
@@ -0,0 +1,54 @@
+package com.cho.navi.ui.recommendation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.cho.navi.data.Post
+import com.cho.navi.databinding.ItemRecommedationPostBinding
+
+class RecommendationPostAdapter :
+    RecyclerView.Adapter<RecommendationPostAdapter.RecommendationPostViewHolder>() {
+
+    private val items = mutableListOf<Post>()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecommendationPostViewHolder {
+        return RecommendationPostViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: RecommendationPostViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    fun submitList(newItems: List<Post>) {
+        items.clear()
+        items.addAll(newItems)
+        notifyDataSetChanged()
+    }
+
+    class RecommendationPostViewHolder private constructor(
+        private val binding: ItemRecommedationPostBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(post: Post) {
+            binding.tvRecommendationTitle.text = post.title
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): RecommendationPostViewHolder {
+                return RecommendationPostViewHolder(
+                    ItemRecommedationPostBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            }
+        }
+    }
+}
