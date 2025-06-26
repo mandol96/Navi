e48d90b81528e18fc48e2a88dacc8ecb5475c9cf
mandol96 <chocan0202@gmail.com>
2025-06-26 20:57:23 +0900

    기능: 사진 선택 가능

--- app/src/main/java/com/cho/navi/ui/addspot/AddSpotFragment.kt
+++ app/src/main/java/com/cho/navi/ui/addspot/AddSpotFragment.kt
@@ -1,16 +1,21 @@
 package com.cho.navi.ui.addspot
 
+import android.app.AlertDialog
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.cho.navi.data.Spot
 import com.cho.navi.databinding.FragmentAddSpotBinding
-import com.cho.navi.util.Constants
+import com.cho.navi.util.ResultKeys
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -22,6 +27,19 @@ class AddSpotFragment : Fragment() {
     private var isValidSpotCategory = false
     private var isValidSpotName = false
     private var isValidSpotDescription = false
+
+    private val selectedImageUris = mutableListOf<Uri>()
+
+    private val pickMultiplePhotos =
+        registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(5)) { uris ->
+            if (uris.isNotEmpty()) {
+                selectedImageUris.clear()
+                selectedImageUris.addAll(uris)
+                binding.ibUploadImage.setImageURI(uris.first())
+            } else {
+                Toast.makeText(requireContext(), "이미지를 선택하지 않았습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -48,11 +66,15 @@ class AddSpotFragment : Fragment() {
             findNavController().navigate(action)
         }
 
+        binding.ibUploadImage.setOnClickListener {
+            pickMultiplePhotos.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+
         parentFragmentManager.setFragmentResultListener(
-            Constants.SELECT_SPOT_RESULT,
+            ResultKeys.SELECT_SPOT_RESULT,
             viewLifecycleOwner
         ) { _, bundle ->
-            val address = bundle.getString(Constants.SELECTED_ADDRESS)
+            val address = bundle.getString(ResultKeys.SELECTED_ADDRESS)
             if (address.isNullOrBlank()) {
                 binding.tvSpotOpenMap.visibility = View.VISIBLE
                 binding.tvSpotAddress.visibility = View.GONE
