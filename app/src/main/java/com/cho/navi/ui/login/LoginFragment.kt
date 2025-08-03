60d55517c8450139ebd8088c7a974c885e3a96c2
mandol96 <chocan0202@gmail.com>
2025-08-03 21:19:34 +0900

    기능: 회원가입 클릭 시 화면 이동

--- app/src/main/java/com/cho/navi/ui/login/LoginFragment.kt
+++ app/src/main/java/com/cho/navi/ui/login/LoginFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.cho.navi.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -19,6 +20,15 @@ class LoginFragment : Fragment() {
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvRegister.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginToRegister()
+            findNavController().navigate(action)
+        }
     }
 
     override fun onDestroyView() {
