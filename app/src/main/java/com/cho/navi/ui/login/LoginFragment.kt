18876e86a7ebc1b188965517519f6a8a07f42558
mandol96 <chocan0202@gmail.com>
2025-08-04 15:33:21 +0900

    기능: 이미지 버튼 생성

--- app/src/main/java/com/cho/navi/ui/login/LoginFragment.kt
+++ app/src/main/java/com/cho/navi/ui/login/LoginFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.cho.navi.data.AuthRepository
 import com.cho.navi.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment : Fragment() {
 
